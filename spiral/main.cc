90e474ab2dfeb97eb0b5eb1bd94197301a1d6ce6
fuse <fzorto@gmail.com>
2020-05-09 20:23:01 -0300

    move common code to library

--- spiral/main.cc
+++ spiral/main.cc
@@ -188,6 +188,12 @@ int main()
             glfwSetWindowShouldClose(window, GL_TRUE);
     });
 
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(
+        [](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei /*length*/, const GLchar *message,
+           const void * /*user*/) { std::cout << source << ':' << type << ':' << severity << ':' << message << '\n'; },
+        nullptr);
+
 #ifdef DUMP_FRAMES
     constexpr auto total_frames = CycleDuration * FramesPerSecond;
     auto frame_num = 0;
