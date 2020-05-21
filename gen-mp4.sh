179fd774892960c112b02991c95634491557757f
fuse <fzorto@gmail.com>
2020-05-21 09:15:15 -0300

    fix gen-mp4

--- gen-mp4.sh
+++ gen-mp4.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -framerate 40 -i "%05d.ppm" -vf "scale=720:720,vflip" -c:v libx264 -vf format=yuv420p clip.mp4
+ffmpeg -framerate 40 -i "%05d.ppm" -vf "format=yuv420p,scale=720:720,vflip" -c:v libx264 clip.mp4
